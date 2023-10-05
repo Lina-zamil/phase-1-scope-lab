75fec2151052caf63c6fc469c99eb94194ce72f0
lina abumahfood <lina.abumahfood@gmail.com>
2023-10-05 20:49:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    return customerName.toUpperCase();
+}
+console.log(upperCaseCustomerName());
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    return bestCustomer;
+}
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+    return bestCustomer;
+}
+const leastFavoriteCustomer = 'hi'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "hello"
+}
